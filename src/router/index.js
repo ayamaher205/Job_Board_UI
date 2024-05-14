8b8763c0f5bbadf22fc7d84d4ebd6e8cf4f9eb74
ayamaher205 <aya.maher.m7md@gmail.com>
2024-05-14 20:51:40 +0300

    feat:add tailwind configurations

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import SearchResult from '../views/SearchResult.vue';
 import JobsPage from '../views/JobsPage.vue';
-
+import Sidebar from '../components/Sidebar.vue'
+import EmployerProfile from '../components/EmployerProfile.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -19,7 +20,16 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
     },
-   
+    {
+      path:'/employer',
+      name:'empoyer_profile',
+      component:Sidebar
+    },
+    {
+      path:'/employer-profile',
+      name:'profile',
+      component:EmployerProfile
+    },
     { path: '/jobs', name: 'Jobs', component: JobsPage }
     ,
    
