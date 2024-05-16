3fde3b6766ad42579763f58793b521ac0c8320da
esraamagddi <esraamagdy20133@gmail.com>
2024-05-16 12:09:40 +0300

    feat:add background image

--- src/main.js
+++ src/main.js
@@ -7,12 +7,14 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import Vueform from '@vueform/vueform'
-import vueformConfig from './../vueform.config'
+import vueformConfig from '../vueform.config'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
 app.use(Vueform, vueformConfig)
+
+app.use(router)
 
 app.mount('#app')
