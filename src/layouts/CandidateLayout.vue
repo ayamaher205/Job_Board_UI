5af2156a4d9ae434a699eb27c1548fbe16437b8b
AbdelrahmanElsheikh965 <abdoelsheikh892@gmail.com>
2024-05-18 12:40:53 +0300

    feat: complete candidate layout and formatted files

--- src/layouts/CandidateLayout.vue
+++ src/layouts/CandidateLayout.vue
@@ -1,11 +1,21 @@
 <template>
+  <Header></Header>  
   <router-view></router-view>
+  <Footer></Footer>
 </template>
 
 <script>
+import Header from './Header.vue';
+import Footer from './Footer.vue';
+
 export default {
-  name: 'CandidateLayout'
+  name: 'CandidateLayout',
+  components: {
+    Header,
+    Footer
+  }
 }
+
 </script>
 
 <style scoped>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
-import ApplicationForm from '../views/ApplicationForm.vue'
-import JobsPage from '../views/JobsPage.vue'
-import SearchResult from '../views/SearchResult.vue'
-import ShowApplications from '@/views/ShowApplications.vue'
-import EditApplication from './../views/EditApplication.vue'
-import ListApplications from '@/views/ListApplications.vue'
-import DeleteApplication from './../views/DeleteApplication.vue'
-import JobDetails from '../views/JobDetails.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import AboutView from "../views/AboutView.vue";
+import ApplicationForm from "../views/ApplicationForm.vue";
+import JobsPage from "../views/JobsPage.vue";
+import SearchResult from "../views/SearchResult.vue";
+import ShowApplications from "@/views/ShowApplications.vue";
+import EditApplication from "./../views/EditApplication.vue";
+import ListApplications from "@/views/ListApplications.vue";
+import DeleteApplication from "./../views/DeleteApplication.vue";
+import JobDetails from "../views/JobDetails.vue";
 import Register from "../components/CandidateComponents/Register.vue";
 import Login from "../components/CandidateComponents/Login.vue";
 import ProfileStore from "../components/CandidateComponents/ProfileStore.vue";
@@ -33,49 +33,46 @@ const router = createRouter({
       component: ProfileStore,
     },
     {
-      path: '/get-applications',
-      name: 'getApplications',
-      component: ListApplications
+      path: "/get-applications",
+      name: "getApplications",
+      component: ListApplications,
     },
     {
-      path: '/edit-applications/:id',
-      name: 'editApplications',
-      component: EditApplication
+      path: "/edit-applications/:id",
+      name: "editApplications",
+      component: EditApplication,
     },
     {
-      path: '/show-applications/:id',
-      name: 'showApplications',
-      component: ShowApplications
+      path: "/show-applications/:id",
+      name: "showApplications",
+      component: ShowApplications,
     },
     {
-      path: '/delete-applications/:id',
-      name: 'deleteApplications',
-      component: DeleteApplication
+      path: "/delete-applications/:id",
+      name: "deleteApplications",
+      component: DeleteApplication,
     },
     {
-      path: '/jobs',
-      name: 'jobs',
-      component: JobsPage
+      path: "/jobs",
+      name: "jobs",
+      component: JobsPage,
     },
     {
-      path: '/searchResult',
-      name: 'searchResult',
-      component: SearchResult
+      path: "/searchResult",
+      name: "searchResult",
+      component: SearchResult,
     },
-   
-    { path: '/jobs', name: 'Jobs', component: JobsPage }
-    ,
-   
-    { path: '/searchResult', name: 'searchResult', component: SearchResult },
 
-    {path: '/posts/:id', name: 'job-details', component: JobDetails},
+    { path: "/jobs", name: "Jobs", component: JobsPage },
+    { path: "/searchResult", name: "searchResult", component: SearchResult },
+
+    { path: "/posts/:id", name: "job-details", component: JobDetails },
     {
       path: "/candidate/profile/me",
-    name: "candidate_profile_view",
-    component: UpdateProfile,
-    }
-  ]
-})
+      name: "candidate_profile_view",
+      component: UpdateProfile,
+    },
+  ],
+});
 
 export default router;
-   
