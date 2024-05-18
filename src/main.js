bde618e13097abd26c72e3f6666e29ca6f9fc566
AbdelrahmanElsheikh965 <abdoelsheikh892@gmail.com>
2024-05-18 23:56:05 +0300

    fix: bugs

--- src/main.js
+++ src/main.js
@@ -1,22 +1,23 @@
  
+import 'flowbite'
 import 'sweetalert2/dist/sweetalert2.min.css';
 import './assets/style.css'
 import '../node_modules/flowbite-vue/dist/index.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-icons/font/bootstrap-icons.css';
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faEllipsisV } from '@fortawesome/free-solid-svg-icons';
+
 import router from './router'
 import Vueform from '@vueform/vueform'
 import CandidateLayout from './layouts/CandidateLayout.vue'
 import VueSweetalert2 from 'vue-sweetalert2';
 import vueformConfig from '../vueform.config'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'flowbite'
 
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faEllipsisV } from '@fortawesome/free-solid-svg-icons';
 
 library.add(faEllipsisV);
 // const app = createApp(App)
